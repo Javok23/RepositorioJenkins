d1867afe84792e3e24317de289ec4511e65abaea
Javok23 <130308615+Javok23@users.noreply.github.com>
2023-04-17 22:06:52 +0200

    Add files via upload

---
+++ src/main/java/ProjectMaven72/pmv72/Pmv72Application.java
@@ -0,0 +1,13 @@
+package ProjectMaven72.pmv72;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pmv72Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pmv72Application.class, args);
+	}
+
+}
---
+++ src/test/java/ProjectMaven72/pmv72/Pmv72ApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjectMaven72.pmv72;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pmv72ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
