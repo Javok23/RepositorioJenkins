06fffc145355352a025d82d6bb17365c9cb7c541
Javok23 <130308615+Javok23@users.noreply.github.com>
2023-04-17 22:14:04 +0200

    Add files via upload

---
+++ src/test/java/ProjectMaven73/pmv73/Pmv73ApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjectMaven73.pmv73;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pmv73ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
