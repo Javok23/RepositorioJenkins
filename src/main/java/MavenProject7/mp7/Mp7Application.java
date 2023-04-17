fede34308728d8d3d156a22f4252caafe8d57b8a
Javok23 <130308615+Javok23@users.noreply.github.com>
2023-04-17 21:24:30 +0200

    Add files via upload

---
+++ src/main/java/MavenProject7/mp7/Mp7Application.java
@@ -0,0 +1,13 @@
+package MavenProject7.mp7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp7Application.class, args);
+	}
+
+}
