d0679982f22e482000f47925b074b98bf75b735b
Javok23 <130308615+Javok23@users.noreply.github.com>
2023-04-17 22:04:05 +0200

    Add files via upload

---
+++ src/main/java/ProjectMaven71/pmv71/Pmv71Application.java
@@ -0,0 +1,13 @@
+package ProjectMaven71.pmv71;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pmv71Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pmv71Application.class, args);
+	}
+
+}
