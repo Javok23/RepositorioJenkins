85714d402896f45f96e7ced40eaacf75eac02228
Javok23 <130308615+Javok23@users.noreply.github.com>
2023-04-17 21:47:45 +0200

    Add files via upload

---
+++ src/main/java/ProjectMaven7/pmv7/Pmv7Application.java
@@ -0,0 +1,13 @@
+package ProjectMaven7.pmv7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pmv7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pmv7Application.class, args);
+	}
+
+}
---
+++ src/test/java/ProjectMaven7/pmv7/Pmv7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjectMaven7.pmv7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pmv7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
